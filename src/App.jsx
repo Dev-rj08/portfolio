14431224122761d54e337d4a2de4b64b71e85855
Ragavendra Raja Jagathish <Dev-rj08>
2024-08-19 19:14:33 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -45,8 +45,6 @@ function App() {
                             <Experience />
                             <Projects />
                             <Contact /> 
-
-                            
                         </>
                     )}
                 </div>
